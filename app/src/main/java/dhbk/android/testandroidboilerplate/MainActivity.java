5d06d61a199d602f6aa879f8b39a66d019048b7c
Phong <p7157568@gmail.com>
2016-08-15 14:57:36 +0700

    Initial commit

---
+++ app/src/main/java/dhbk/android/testandroidboilerplate/MainActivity.java
@@ -0,0 +1,13 @@
+package dhbk.android.testandroidboilerplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
